766ef14eaf20cfe3eb89d457a843cd1a063a8cee
Rishabhjaiswal2006 <72344228+Rishabhjaiswal2006@users.noreply.github.com>
2020-11-01 23:23:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -57,7 +57,7 @@ function draw() {
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     
-	Matter.Body.setStatic(packageBody,false,{restitution:0.2,friction:10});
+	Matter.Body.setStatic(packageBody,false);
 	
 	
   }
